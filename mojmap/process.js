74bcd62988e95686743008197fdec588731f5b38
adrian154 <94568a@gmail.com>
2022-06-30 12:12:18 -0700

    tinker with mojmap

--- mojmap/process.js
+++ mojmap/process.js
@@ -2,11 +2,7 @@
 const fs = require("fs"),
       mappingsText = fs.readFileSync("evil_nonfree/client.txt", "utf-8");
 
-// 12:30 AM variable names really hit different
-// oh, right, 24 hour time. 00:30 variable naming at play here.
-// (is that right? too late to tell. or, well, I guess too early.)
 const dict = new Map();
-
 const addEntry = (str, thing) => {
     const list = dict.get(str);
     if(list)
@@ -18,7 +14,7 @@ const addEntry = (str, thing) => {
 // parser state
 let curClassName, curFQCN;
 
-console.log("building dictionary..");
+console.log("building dictionary...");
 for(const rawLine of mappingsText.split('\n')) {
     
     const line = rawLine.trim();
@@ -29,27 +25,27 @@ for(const rawLine of mappingsText.split('\n')) {
     // regex for parsing? unthinkable.
     // god, I need to finish that parser combinator library so bad
     // in case it isn't obvious this code isn't robust at all, but who cares
-    const classNameMatch = line.match(/(?<package>.+)\.(?<className>.+) -> (?<obfuscatedClassName>.+):/);
-    if(classNameMatch) {
-        const {package, className, obfuscatedClassName} = classNameMatch.groups;
+    const classMatch = line.match(/(?<package>.+)\.(?<className>.+) -> (?<obfuscated>.+):/);
+    if(classMatch) {
+        const {package, className, obfuscated} = classMatch.groups;
         const fqcn = package + "." + className;
         curClassName = className;
         curFQCN = fqcn;
-        const thing = {type: "class", fqcn, obfuscated: obfuscatedClassName};
-        addEntry(fqcn, thing);
-        addEntry(className, thing);
+        const entry = {type: "class", fqcn, obfuscated};
+        addEntry(fqcn, entry);
+        addEntry(className, entry);
         continue;
     }
 
-    const methodNameMatch = line.match(/(?<startLine>\d+):(?<endLine>\d+):(?<retType>.+) (?<methodName>.+)\((?<signature>.*)\) -> (?<obfuscatedMethodName>.+)/);
-    if(methodNameMatch) {
-        const {startLine, endLine, retType, methodName, signature, obfuscatedMethodName} = methodNameMatch.groups;
+    const methodMatch = line.match(/(?<startLine>\d+):(?<endLine>\d+):(?<retType>.+) (?<methodName>.+)\((?<signature>.*)\) -> (?<obfuscated>.+)/);
+    if(methodMatch) {
+        const {startLine, endLine, retType, methodName, signature, obfuscated} = methodMatch.groups;
 
         // further parse the method name
         const params = signature.split(',').map(param => param.trim());
         
         // method descriptor TODO
-        const thing = {
+        const entry = {
             type: "method",
             params,
             class: curFQCN,
@@ -57,49 +53,48 @@ for(const rawLine of mappingsText.split('\n')) {
             startLine,
             endLine,
             name: methodName,
-            obfuscated: obfuscatedMethodName
+            obfuscated
         };
 
-        addEntry(methodName, thing);
-        addEntry(curClassName + "." + methodName, thing);
-        addEntry(curFQCN + "." + methodName, thing);
+        addEntry(methodName, entry);
+        addEntry(curClassName + "." + methodName, entry);
+        addEntry(curFQCN + "." + methodName, entry);
         continue;
     }
 
-    const fieldNameMatch = line.match(/(?<type>.+) (?<fieldName>.+) -> (?<obfuscatedFieldName>.+)/);
-    if(fieldNameMatch) {
-        const {type, fieldName, obfuscatedFieldName} = fieldNameMatch.groups;
-        const thing = {
+    const fieldMatch = line.match(/(?<type>.+) (?<fieldName>.+) -> (?<obfuscated>.+)/);
+    if(fieldMatch) {
+        const {type, fieldName, obfuscated} = fieldMatch.groups;
+        const entry = {
             type: "field",
+            name: fieldName,
             fieldtype: type,
-            obfuscated: obfuscatedFieldName
+            obfuscated
         };
-        addEntry(fieldName, thing);
-        addEntry(curClassName + "." + fieldName, thing);
-        addEntry(curFQCN + "." + fieldName, thing);
+        addEntry(fieldName, entry);
+        addEntry(curClassName + "." + fieldName, entry);
+        addEntry(curFQCN + "." + fieldName, entry);
     }
 
 }
 
 // if we naiively JSONify the dictionary it will be absolutely huge because of all the duplicate values
 // we can achieve pretty good space reduction by reducing the redundancy
-
 console.log("condensing dictionary..");
-const things = [], flatDict = [];
+const objects = [], flattenedDict = [];
 
 for(const [term, entries] of dict) {
-    const updated = entries.map(entry => {
-        const index = things.indexOf(entry);
-        if(index >= 0) {
-            return index;
+    flattenedDict.push([term, entries.map(entry => {
+        const index = objects.indexOf(entry);
+        if(index < 0) {
+            objects.push(entry);
+            return objects.length - 1;
         }
-        things.push(entry);
-        return things.length - 1;
-    });
-    flatDict.push([term, updated]);
+        return index;
+    })]);
 }
 
 fs.writeFileSync("dictionary.json", JSON.stringify({
-    dictionary: flatDict,
-    things
+    dictionary: flattenedDict,
+    objects
 }));
