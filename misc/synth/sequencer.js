e0bf9eb4f1036521795fe19bc9fcc96ef7fe26f6
adrian <nyancatgaming9999@gmail.com>
2023-04-28 11:56:07 -0700

    guidelines

--- misc/synth/sequencer.js
+++ misc/synth/sequencer.js
@@ -21,37 +21,70 @@ let horizScroll = 0,
 const draw = () => {
 
     // draw background
-    ctx.fillStyle = "#222222";
+    ctx.fillStyle = "#333333";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.translate(0, -vertScroll);
+
+    // draw horizontal lines
+    ctx.strokeStyle = "rgba(0, 0, 0, 25%)";
+    for(let key = 0; key < 84; key++) {
+        ctx.beginPath();
+        ctx.moveTo(0, key * ROW_HEIGHT);
+        ctx.lineTo(canvas.width, key * ROW_HEIGHT);
+        ctx.stroke();
+    }
+
+    // draw barlines
+    const START_TICK = Math.floor(horizScroll),
+          END_TICK = START_TICK + (canvas.width - PIANO_WIDTH) / tickSize;
+    for(let tick = START_TICK; tick < END_TICK; tick++) {
+        if(tick % 32 == 0) {
+            ctx.strokeStyle = (tick / 32) % song.rhythm == 0 ? "rgba(0, 0, 0, 50%)" : "rgba(0, 0, 0, 25%)";
+            ctx.beginPath();
+            ctx.moveTo(tick * tickSize - horizScroll + PIANO_WIDTH, 0);
+            ctx.lineTo(tick * tickSize - horizScroll + PIANO_WIDTH, 84 * ROW_HEIGHT);
+            ctx.stroke();
+        }
+    }
 
     // draw keyboard
     ctx.font = "14px Arial";
     ctx.textAlign = "right";
     ctx.strokeStyle = "#000000";
     for(let key = 0; key <= 84; key++) {
+
         const octave = Math.floor(key / 12) + 1;
         const NOTE = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"][key % 12];
+        
+        // draw key
         if(NOTE.includes("#"))
             ctx.fillStyle = "#000000";
         else
             ctx.fillStyle = "#ffffff";
         ctx.fillRect(0, key * ROW_HEIGHT, PIANO_WIDTH, ROW_HEIGHT);
-        ctx.strokeRect(0, key * ROW_HEIGHT, PIANO_WIDTH, ROW_HEIGHT);
+        
+        // draw line dividing keys 
+        ctx.beginPath();
+        ctx.moveTo(0, key * ROW_HEIGHT + 0.5);
+        ctx.lineTo(PIANO_WIDTH, key * ROW_HEIGHT + 0.5);
+        ctx.stroke();
+
+        // draw note name
         if(NOTE.includes("#"))
             ctx.fillStyle = "#ffffff";
         else
             ctx.fillStyle = "#000000";
         ctx.fillText(NOTE + octave, PIANO_WIDTH - 4, key * ROW_HEIGHT + ROW_HEIGHT * 0.65);
+
     }
 
     // draw notes
-    for(let tick = Math.floor(horizScroll); tick < horizScroll + (canvas.width - PIANO_WIDTH) / tickSize; tick++) {
+    for(let tick = START_TICK; tick < END_TICK; tick++) {
         const notes = song.notes.get(tick);
         if(notes) {
             for(const note of notes) {
-                ctx.fillStyle = "#ffffff";
+                ctx.fillStyle = note.instrument.color;
                 ctx.fillRect(tick * tickSize - horizScroll + PIANO_WIDTH, (note.note - 24) * ROW_HEIGHT, note.length * tickSize, ROW_HEIGHT);
             }
         }
--- misc/synth/synth.js
+++ misc/synth/synth.js
@@ -1,4 +1,11 @@
 // 1 tick = 1/32 of a beat. `notes` maps ticks to a list of notes that *start* on that tick.
 let song = {
-    notes: new Map()
+    notes: new Map(),
+    rhythm: 4
 };
+
+let instruments = [
+    {name: "beep", type: "square", attack: 0.015, delay: 0.05, color: "#4e61d8"}
+];
+
+song.notes.set(100, [{note: 30, length: 32, instrument: instruments[0]}]);
