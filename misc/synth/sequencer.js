90a45450d3d8a96bce3a8e0a131ee746a682fa8f
adrian <nyancatgaming9999@gmail.com>
2023-04-28 14:03:41 -0700

    octave lines

--- misc/synth/sequencer.js
+++ misc/synth/sequencer.js
@@ -16,7 +16,7 @@ const resizeCanvas = () => {
 // tickScale = pixels/tick
 let horizScroll = 0,
     vertScroll = 600,
-    tickSize = 2; 
+    tickSize = 4; 
 
 const draw = () => {
 
@@ -36,13 +36,15 @@ const draw = () => {
         ctx.stroke();
     }
 
-    const START_TICK = Math.floor(horizScroll / tickSize),
-          END_TICK = START_TICK + canvas.width / tickSize;
+    const startTick = Math.floor(horizScroll / tickSize),
+          endTick = startTick + canvas.width / tickSize;
+
+    const noteLen = 48 / song.timeSig.lower;
 
     // draw barlines
-    for(let tick = START_TICK; tick < END_TICK; tick++) {
-        if(tick % 32 == 0) {
-            ctx.strokeStyle = (tick / 32) % song.rhythm == 0 ? "rgba(0, 0, 0, 50%)" : "rgba(0, 0, 0, 25%)";
+    for(let tick = startTick; tick < endTick; tick++) {
+        if(tick % noteLen == 0) {
+            ctx.strokeStyle = (tick / noteLen) % song.timeSig.upper == 0 ? "rgba(0, 0, 0, 50%)" : "rgba(0, 0, 0, 10%)";
             ctx.beginPath();
             ctx.moveTo(tick * tickSize - horizScroll, 0);
             ctx.lineTo(tick * tickSize - horizScroll, 84 * ROW_HEIGHT);
@@ -50,17 +52,36 @@ const draw = () => {
         }
     }
 
+    // draw octave lines
+    for(let i = 0; i < 84; i += 12) {
+        ctx.strokeStyle = "rgba(0, 0, 0, 30%)";
+        ctx.beginPath();
+        ctx.moveTo(0, i * ROW_HEIGHT);
+        ctx.lineTo(canvas.width, i * ROW_HEIGHT);
+        ctx.stroke();
+    }
+
     // draw notes
     // FIXME: notes longer than 256 ticks have a tendency to disappear off the left
-    for(let tick = START_TICK - 256; tick < END_TICK; tick++) {
+    for(let tick = startTick - 256; tick < endTick; tick++) {
         const notes = song.notes.get(tick);
         if(notes) {
             for(const note of notes) {
-                if(tick + note.length > START_TICK) {
+                if(tick + note.length > startTick) {
                     ctx.fillStyle = note.instrument.color;
-                    ctx.fillRect(tick * tickSize - horizScroll, (83 - note.note + 24) * ROW_HEIGHT, note.length * tickSize, ROW_HEIGHT);
+                    ctx.fillRect(tick * tickSize - horizScroll + 1, (83 - note.note + 24) * ROW_HEIGHT + 1, note.length * tickSize - 2, ROW_HEIGHT - 2);
                 }
             }
+        }
+    }
+
+    // draw bar number
+    ctx.fillStyle = "rgb(255, 255, 255, 20%)";
+    ctx.font = "20px sans-serif";
+    ctx.textAlign = "left";
+    for(let tick = startTick; tick < endTick; tick++) {
+        if(tick % (noteLen * song.timeSig.upper) == 0) {
+            ctx.fillText(tick / noteLen / song.timeSig.upper, tick * tickSize - horizScroll + 4, vertScroll + 22);
         }
     }
 
@@ -97,23 +118,17 @@ const draw = () => {
 
     }
 
-    // draw bar number
-    ctx.fillStyle = "rgb(255, 255, 255, 10%)";
-    ctx.font = "20px sans-serif";
-    ctx.textAlign = "left";
-    for(let tick = START_TICK; tick < END_TICK; tick++) {
-        if(tick % (32 * song.rhythm) == 0) {
-            ctx.fillText(tick / 32 / song.rhythm, tick * tickSize - horizScroll + 4, vertScroll + 22);
-        }
-    }
-
 };
+
+let noteLength = 12;
 
 canvas.addEventListener("wheel", event => {
     vertScroll = Math.max(0, Math.min(84 * ROW_HEIGHT - canvas.height, vertScroll + event.deltaY));
     horizScroll = Math.max(0, horizScroll + event.deltaX);
     draw();
 });
+
+// 
 
 resizeCanvas();
 window.addEventListener("resize", resizeCanvas);
--- misc/synth/synth.js
+++ misc/synth/synth.js
@@ -1,17 +1,9 @@
-// 1 tick = 1/32 of a beat. `notes` maps ticks to a list of notes that *start* on that tick.
+// 1 tick = 1/48 of a beat. `notes` maps ticks to a list of notes that *start* on that tick.
 let song = {
     notes: new Map(),
-    rhythm: 4
+    timeSig: {upper: 5, lower: 4}
 };
 
 let instruments = [
     {name: "beep", type: "square", attack: 0.015, delay: 0.05, color: "#4e61d8"}
 ];
-
-for(let i = 0; i < 100; i++) {
-    song.notes.set(i * 50 + 100, [
-        {note: 60 + i, length: 32, instrument: instruments[0]},
-        {note: 50 + i, length: 64, instrument: instruments[0]},
-        {note: 40 + i, length: 8, instrument: instruments[0]}
-    ]);
-}
