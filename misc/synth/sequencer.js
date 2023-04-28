844ad81e6c05e32c75b39b9bca205cc3512f1ccc
adrian <nyancatgaming9999@gmail.com>
2023-04-28 11:41:35 -0700

    draw notes

--- misc/synth/sequencer.js
+++ misc/synth/sequencer.js
@@ -1,4 +1,5 @@
-const ROW_HEIGHT = 24 * devicePixelRatio;
+const ROW_HEIGHT = 24 * devicePixelRatio,
+      PIANO_WIDTH = 90;
 
 const canvas = document.getElementById("sequencer"),
       ctx = canvas.getContext("2d", {alpha: false});
@@ -15,7 +16,7 @@ const resizeCanvas = () => {
 // tickScale = pixels/tick
 let horizScroll = 0,
     vertScroll = 0,
-    tickSize = 8; 
+    tickSize = 2; 
 
 const draw = () => {
 
@@ -23,28 +24,37 @@ const draw = () => {
     ctx.fillStyle = "#222222";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    ctx.translate(0, -vertScroll);
+
     // draw keyboard
     ctx.font = "14px Arial";
     ctx.textAlign = "right";
-    for(let key = 0; key < 84; key++) {
+    ctx.strokeStyle = "#000000";
+    for(let key = 0; key <= 84; key++) {
         const octave = Math.floor(key / 12) + 1;
         const NOTE = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"][key % 12];
         if(NOTE.includes("#"))
             ctx.fillStyle = "#000000";
         else
             ctx.fillStyle = "#ffffff";
-        ctx.fillRect(0, key * ROW_HEIGHT, 90, ROW_HEIGHT);
+        ctx.fillRect(0, key * ROW_HEIGHT, PIANO_WIDTH, ROW_HEIGHT);
+        ctx.strokeRect(0, key * ROW_HEIGHT, PIANO_WIDTH, ROW_HEIGHT);
         if(NOTE.includes("#"))
             ctx.fillStyle = "#ffffff";
         else
             ctx.fillStyle = "#000000";
-        ctx.fillText(NOTE + octave, 86, key * ROW_HEIGHT + ROW_HEIGHT * 0.65);
+        ctx.fillText(NOTE + octave, PIANO_WIDTH - 4, key * ROW_HEIGHT + ROW_HEIGHT * 0.65);
     }
 
     // draw notes
-    for(let tick = Math.floor(horizScroll); tick < horizScroll + canvas.width / tickSize; tick++) {
+    for(let tick = Math.floor(horizScroll); tick < horizScroll + (canvas.width - PIANO_WIDTH) / tickSize; tick++) {
         const notes = song.notes.get(tick);
-        // ... draw ...
+        if(notes) {
+            for(const note of notes) {
+                ctx.fillStyle = "#ffffff";
+                ctx.fillRect(tick * tickSize - horizScroll + PIANO_WIDTH, (note.note - 24) * ROW_HEIGHT, note.length * tickSize, ROW_HEIGHT);
+            }
+        }
     }
 
 };
